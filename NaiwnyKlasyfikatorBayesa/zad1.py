e461ddf3b546a3db9eda5a43529d68e319178a69
Bartek Dobrzanski <baarti02@gmail.com>
2025-01-03 11:22:31 +0100

    Poprawienie zadania1 lab3, podział na zbiór testowy nie miał tam sensu

--- NaiwnyKlasyfikatorBayesa/zad1.py
+++ NaiwnyKlasyfikatorBayesa/zad1.py
@@ -20,24 +20,9 @@ y = df['Zagrano mecz']
 print(X)
 print(y)
 
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
-
 # Klasyfikator Bayes'a
 model = GaussianNB()
-model.fit(X_train, y_train)
-
-y_pred = model.predict(X_test)
-print(X_test)
-
-# Wyniki
-print("Klasyfikator Bayesa:")
-print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
-print(classification_report(y_test, y_pred))
-print("F1 score: ", f1_score(y_test, y_pred, average="weighted"))
-
-print(y_test)
-print(y_pred)
+model.fit(X, y)
 
 # Wynik dla zapytania z pdf'a
 new_data = {
@@ -60,22 +45,23 @@ print("\nPrzewidywany wynik (czy zagrają mecz):", predicted_label[0])
 
 
 # k-NN (odległość euklidesowa)
-knn_euclidean = KNeighborsClassifier(metric='euclidean', n_neighbors=2)
-knn_euclidean.fit(X_train, y_train)
-y_pred_knn_euclidean = knn_euclidean.predict(X_test)
+knn_euclidean = KNeighborsClassifier(metric='euclidean', n_neighbors=3)
+knn_euclidean.fit(X, y)
+y_pred_knn_euclidean = knn_euclidean.predict(new_df)
+euk_predicted = label_encoders['Zagrano mecz'].inverse_transform(y_pred_knn_euclidean)
 
 # Wyniki dla odległości euklidesowej
-print("k-NN z odległością euklidesową:")
-print(f"Accuracy: {accuracy_score(y_test, y_pred_knn_euclidean)}")
-print(classification_report(y_test, y_pred_knn_euclidean))
+print("\nk-NN z odległością euklidesową:")
+print("Przewidywany wynik (czy zagrają mecz):", euk_predicted[0])
 
 
 # k-NN (odległość miejska)
-knn_manhattan = KNeighborsClassifier(metric='manhattan', n_neighbors=2)
-knn_manhattan.fit(X_train, y_train)
-y_pred_knn_manhattan = knn_manhattan.predict(X_test)
+knn_manhattan = KNeighborsClassifier(metric='manhattan', n_neighbors=3)
+knn_manhattan.fit(X, y)
+y_pred_knn_manhattan = knn_manhattan.predict(new_df)
+manh_predicted = label_encoders['Zagrano mecz'].inverse_transform(y_pred_knn_manhattan)
 
 # Wyniki dla odległości miejskiej
-print("k-NN z odległością miejską:")
-print(f"Accuracy: {accuracy_score(y_test, y_pred_knn_manhattan)}")
-print(classification_report(y_test, y_pred_knn_manhattan))
+print("\nk-NN z odległością miejską:")
+print("Przewidywany wynik (czy zagrają mecz):", manh_predicted[0])
+
