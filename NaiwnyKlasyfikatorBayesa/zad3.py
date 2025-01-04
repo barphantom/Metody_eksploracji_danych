0dd091a477e719b65a73b7bda4563c9d5e26e104
Luki <farcryx02@gmail.com>
2025-01-04 10:01:30 +0100

    Lekka poprawka wyświetlania etykiet na końcu programu

--- NaiwnyKlasyfikatorBayesa/zad3.py
+++ NaiwnyKlasyfikatorBayesa/zad3.py
@@ -86,7 +86,12 @@ new_post_tfidf = vectorizer.transform([new_post_processed])
 prediction = model.predict(new_post_tfidf)
 print(f"Predykcja dla nowego posta: {prediction[0]}")
 
-print("\n")
-for sym in range(len(y_pred)):
-    print(f"Post: {X_test.iloc[sym]} | Etykieta rzeczywista: {y_test.iloc[sym]} | Etykieta przewidziana: {y_pred[sym]}")
+print("\n") # Poprawiłem czytelność wyników przez DataFrame
+results_df = pd.DataFrame({
+    'Post': X_test,
+    'Etykieta rzeczywista': y_test,
+    'Etykieta przewidziana': y_pred
+})
+
+print(results_df)
 
